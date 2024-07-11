5a60e29aa094ddd20677280fe532a7fea3d4653d
RaghavG19 <imraghavgandhi@gmail.com>
2024-07-11 14:29:02 +0530

    Update asyncawait.js

--- asyncawait.js
+++ asyncawait.js
@@ -27,6 +27,27 @@
         // The await keyword is used to wait for a promise to resolve. It can only be used within an async block.
         // Execution Pause: Await makes the code wait until the promise returns a result, allowing for cleaner and
         // more manageable asynchronous code.
+        //The await keyword can only be used inside an async function.
         // Syntax
         // let value = await promise;
+        const getData1= async () => {
+            let y = await "Hello World";
+            console.log(y);
+        }
         
+        console.log(1);
+        getData();
+        console.log(2);
+        // Output
+        // 1
+        // 2
+        // Hello World
+
+        // The async keyword transforms a regular JavaScript function into an asynchronous function, causing it to 
+        //return a Promise.
+        // The await keyword is used inside an async function to pause its execution and wait
+        // for a Promise to resolve before continuing.
+        // The code runs console.log(1) first.
+        // Then it calls the getData function, but pauses when it hits await.
+        // While paused, console.log(2) runs.
+        // Finally, getData resumes and prints "Hello World".
